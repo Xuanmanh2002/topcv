56c316b000dba46cd3a2aff7103edd224c2688f1
Xuanmanh2002 <nguyenxuanmanh2k2@gmail.com>
2024-09-27 09:12:12 +0700

    post

--- src/components/layout/JobList.tsx
+++ src/components/layout/JobList.tsx
@@ -6,21 +6,38 @@ interface Job {
   location: string;
   salary: string;
   company: string;
-  logots: string; 
+  logots: string;
 }
 
 const jobs: Job[] = [
-  { title: 'Nhân Viên Kế Toán', location: 'Hà Nội', salary: 'Trên 10 triệu', company: 'Công ty XYZ', logots: 'https://example.com/logo1.png' },
-  { title: 'Nhân Viên Tư Vấn', location: 'Hồ Chí Minh', salary: '15 - 30 triệu', company: 'Công ty ABC', logots: 'https://example.com/logo2.png' },
+  { title: 'Nhân Viên Kế Toán', location: 'Hà Nội', salary: 'Trên 10 triệu', company: 'Công ty XYZ', logots: 'https://cdn-new.topcv.vn/unsafe/200x/https://static.topcv.vn/company_logos/ngan-hang-tmcp-viet-nam-thinh-vuong-vpbank-63e1cb5539e62.jpg' },
+  { title: 'Nhân Viên Tư Vấn', location: 'Hồ Chí Minh', salary: '15 - 30 triệu', company: 'Công ty ABC', logots: 'https://cdn-new.topcv.vn/unsafe/200x/https://static.topcv.vn/company_logos/ngan-hang-tmcp-viet-nam-thinh-vuong-vpbank-63e1cb5539e62.jpg' },
   // Add more jobs here
 ];
 
 const JobList: React.FC = () => {
   return (
-    <div className="job-list">
-      {jobs.map((job, index) => (
-        <JobItem key={index} job={job} />
-      ))}
+    <div className='container'>
+      <div className='job'>
+        <div className="title-job">
+          <div className="title-main">
+            <div className="title1">
+              <h1>Việc làm tốt nhất</h1>
+            </div>
+            <div className="img-title">
+              <img src='https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/v4/image/welcome/feature-job/label-toppy-ai.png'></img>
+            </div>
+          </div>
+          <div>
+            <a>Xem tất cả</a>
+          </div>
+        </div>
+        <div className="job-list">
+          {jobs.map((job, index) => (
+            <JobItem key={index} job={job} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,9 +1,9 @@
-// components/SearchBar.js
 import { useState } from 'react';
-import { EnvironmentOutlined, DownOutlined } from '@ant-design/icons';
+import { EnvironmentOutlined, DownOutlined, ShoppingOutlined, SearchOutlined } from '@ant-design/icons';
+import JobList from './JobList';
 
 
-const SearchBar = () => {
+const Main = () => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const handleSearch = () => {
@@ -11,15 +11,16 @@ const SearchBar = () => {
     };
 
     return (
-        <div className='main-form'>
-            <div className="header-box">
-                <h1 className='title'>Tìm việc làm nhanh 24h, việc làm mới nhất toàn quốc.</h1>
-                <p className='description'>
-                    "Tiếp cận <span className='number-hight-light'>40,000+</span> tin tuyển dụng việc làm mỗi ngày từ hàng nghìn doanh nghiệp uy tín tại Việt Nam"
-                </p>
-            </div>
+        <div className="container">
+            <div className='main-form '>
+                <div className="header-box">
+                    <h1 className='title'>Tìm việc làm nhanh 24h, việc làm mới nhất toàn quốc.</h1>
+                    <p className='description'>
+                        "Tiếp cận <span className='number-hight-light'>40,000+</span> tin tuyển dụng việc làm mỗi ngày từ hàng nghìn doanh nghiệp uy tín tại Việt Nam"
+                    </p>
+                </div>
 
-            <form className="form-searchBar">
+                <form className="form-searchBar">
                     <div className="item-search">
                         <input
                             className="form-Job"
@@ -31,44 +32,52 @@ const SearchBar = () => {
                     </div>
                     <div className="vertical-line"></div>
                     <div className="item-select">
-                        <EnvironmentOutlined />
+                        <EnvironmentOutlined className="location-icon" />
                         <select className="select-location">
                             <option value="Nghệ An">Nghệ An</option>
                         </select>
-                        <DownOutlined />
+                        <DownOutlined className="down" />
                     </div>
                     <div className="vertical-line"></div>
                     <div className="item-select">
+                        <ShoppingOutlined className="bag-icon" />
                         <select className="select-industry">
                             <option value="Tất cả ngành nghề">Tất cả ngành nghề</option>
                         </select>
+                        <DownOutlined className="down" />
                     </div>
                     <div className="vertical-line"></div>
-                    <button className="search-button" onClick={handleSearch}>Tìm kiếm</button>
-            </form>
+                    <div className='button-search'>
+                        <SearchOutlined className="search-icon" />
+                        <button className="button" onClick={handleSearch}>Tìm kiếm</button>
+                    </div>
+                </form>
 
-            <div className='information'>
-                <div>
-                    <h1>Vị trí chờ bạn khám phá</h1>
-                    <h2>43.400</h2>
+                <div className='information'>
+                    <div className="infor1">
+                        <span className="item1">Vị trí chờ bạn khám phá</span>
+                        <span className="Quantity">42.645</span>
+                    </div>
+                    <div className="infor1">
+                        <span className="item1">Việc làm mới nhất</span>
+                        <span className="Quantity">2.512</span>
+                    </div>
+                    <div className="infor1">
+                        <span className="item1">Cập nhật lúc</span>
+                        <span className="Quantity">08:27 26/09/2024</span>
+                    </div>
                 </div>
-                <div>
-                    <h1>Việc làm mới nhất</h1>
-                    <h2>3.092</h2>
-                </div>
-                <div>
-                    <h1>Cập nhật lúc</h1>
-                    <h2>11:24 25/09/2024</h2>
+
+                <div className="banner">
+                    <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/img/Camp10_CVO_1100x220_1909.png" alt="Banner" />
                 </div>
             </div>
-
-            <div className="banner">
-                <img src="https://cdn-new.topcv.vn/unsafe/https://static.topcv.vn/img/Camp10_CVO_1100x220_1909.png" alt="Banner" />
-            </div>
+            <JobList />
         </div>
+
 
 
     );
 };
 
-export default SearchBar;
+export default Main;
